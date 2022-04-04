e5f755f44d2341a2c400df332c0f29edfb2d402c
fwzgy <3515233295qq.com>
2022-04-04 21:48:27 +0800

    在hot-fix分支上第一次提交代码

--- src/main/java/com/test/AppMain.java
+++ src/main/java/com/test/AppMain.java
@@ -5,6 +5,7 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("第二次修改");
         System.out.println("第三次修改");
+        System.out.println("hot-fix 第一次提交代码");
     }
 
 }
